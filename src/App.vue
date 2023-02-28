4320ab2c0af6a83d89f39f7ba69007be56574958
Buwei Wu <buwu@ucsd.edu>
2023-02-28 15:15:25 -0800

    :tada: 初始化项目。

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>个人简历</h1>
+  <el-button>123</el-button>
+  <el-icon>
+    <i-ep-full-screen></i-ep-full-screen>
+    <i-ep-chat-square></i-ep-chat-square>
+
+  </el-icon>
+</template>
+
+<script setup>
+
+</script>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
